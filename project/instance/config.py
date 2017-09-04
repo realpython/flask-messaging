e3ea5a6867f948a74ca47df9fbd15a41bedf9ebe
Michael Herman <hermanmu@gmail.com>
2017-09-04 13:45:32 -0600

    updated base

--- project/instance/config.py
+++ project/instance/config.py
@@ -7,7 +7,7 @@ class Config(object):
     """Parent configuration class."""
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.getenv('SECRET')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TESTING = False
--- project/tests/test_main.py
+++ project/tests/test_main.py
@@ -15,7 +15,14 @@ class TestMainBlueprint(unittest.TestCase):
         self.app = create_app(config_name='testing')
         self.client = self.app.test_client
 
-    def test_users(self):
+    def test_config(self):
+        """Ensure the /ping route behaves correctly."""
+        print(self.app.config)
+        self.assertTrue(self.app.config['TESTING'])
+        self.assertTrue(self.app.config['DEBUG'])
+        self.assertEqual(self.app.config['SECRET_KEY'], 'change_me')
+
+    def test_main_route(self):
         """Ensure the /ping route behaves correctly."""
         response = self.client().get('/ping')
         data = json.loads(response.data.decode())
